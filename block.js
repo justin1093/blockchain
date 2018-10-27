42859fb37582458505c34ee6bc4a90ebe7c08268
justin1093 <justinmnl@hotmail.com>
2018-10-27 21:25:16 +0530

    Crypto project begins

--- block.js
+++ block.js
@@ -1,17 +1,17 @@
 const SHA256 = require("crypto-js/sha256");
 
 class Block{
-    constructor (index, timestamp,data,previousHash=''){
+    constructor (index, timestamp,transaction,previousHash=''){
         this.index=index;
         this.timestamp = timestamp;
-        this.data = data;
+        this.transaction = transaction;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
         this.nonce = 0;
     }
 
     calculateHash(){
-        return SHA256(this.index+this.timestamp+this.previousHash+JSON.stringify(this.data)+this.nonce).toString();
+        return SHA256(this.index+this.timestamp+this.previousHash+JSON.stringify(this.transaction)+this.nonce).toString();
     }
 
     mineNewBlock(difficulty){
